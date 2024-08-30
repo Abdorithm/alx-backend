5256c3e3ba861b76a2dde9317afc4afb5ea8c222
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-08-30 21:31:20 +0300

    redis op

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Add this to connect the client
+client.connect();
